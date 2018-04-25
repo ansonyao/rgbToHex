c49d1c18943f17d0b8a4fca5210f841c51cbdea7
Anson Yao <anson.yao@icehousecorp.com>
2018-04-24 21:20:03 -0700

    Refactor it as class

--- src/screens/mainPage/index.js
+++ src/screens/mainPage/index.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 import logo from '../../logo.svg';
 import './style.css';
 
-const MainPage = () => (
-    <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div>
+export default class MainPage extends React.Component {
+    render() {
+        return(
+            <div className = "App" >
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <div>
 
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+                <TextField
+                    hintText="Hint Text"
+                    floatingLabelText="Floating Label Text"
+                />
+                <TextField
+                    hintText="Hint Text"
+                    floatingLabelText="Floating Label Text"
+                />
         </div>
-        <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-        <RaisedButton label="Default" />
-    </div>
-);
-
-export default MainPage;
+        )
+    }
+};
