5c751542ae9e334591a7cca6a0ff59cff29d4798
Anson Yao <anson.yao@icehousecorp.com>
2018-04-24 21:51:24 -0700

    Polish style for the textfields

--- src/screens/mainPage/index.js
+++ src/screens/mainPage/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import logo from '../../logo.svg';
 import './style.css';
@@ -7,26 +6,34 @@ import './style.css';
 export default class MainPage extends React.Component {
     render() {
         return(
-            <div className = "App" >
+            <div className = "App" style={{marginBottom: 400}}>
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
+                    <h1 className="App-title">RGB &lt;====&gt; HEX</h1>
                 </header>
                 <div>
+            </div>
 
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-                <TextField
-                    hintText="Hint Text"
-                    floatingLabelText="Floating Label Text"
-                />
-                <TextField
-                    hintText="Hint Text"
-                    floatingLabelText="Floating Label Text"
-                />
-        </div>
+                {/* <div id="Field-Container"> */}
+                    <TextField
+                        className="MyTextField"
+                        hintText="HEX"
+                        floatingLabelText="HEX"
+                        style={{width: 400, height: 150, fontSize: 55}}
+                        floatingLabelFocusStyle={{marginBottom: 40, fontSize: 45}}
+                        hintStyle={{fontSize: 55, marginBottom: 30}}
+                    />
+                    <span/>
+                    <TextField
+                        className="MyTextField"
+                        hintText="RGB"
+                        floatingLabelText="RGB"
+                        style={{width: 400, height: 150, fontSize: 55}}
+                        floatingLabelFocusStyle={{marginBottom: 40, fontSize: 45}}
+                        hintStyle={{fontSize: 55, marginBottom: 30}}
+                    />
+                {/* </div> */}
+            </div>
         )
     }
 };
